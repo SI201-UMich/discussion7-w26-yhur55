ab3a56d90ee52a8e15abc98b4a705684957ab0c3
yhur55 <yhur@umich.edu>
2026-02-25 17:42:41 -0500

    commit 2

--- discussion7.py
+++ discussion7.py
@@ -63,7 +63,28 @@ def calculate_avg_price_by_neighbourhood_group_and_room(listings):
         dict mapping (neighbourhood_group, room_type) -> average_price (float)
         e.g. { ('Downtown', 'Entire home/apt'): 123.45, ... }
     """
-    pass
+    
+price_sum = {}
+price_count = {}
+for listing in listings:
+    neighbourhood_group = listing["neighbourhood_group"]
+    room_type = listing["room_type"]
+    price = float(listing["price"])
+
+    listing_key = (neighbourhood_group, room_type)
+
+    if listing_key in price_sum:
+        price_sum[listing_key] += price
+        price_count[listing_key] += 1
+    else:
+        price_sum[listing_key] = price
+        price_count[listing_key] = 1
+        
+avg_price = {}
+for key in price_sum:
+    avg_price[key] = price_sum[key] / price_count[key]
+
+return avg_price
 
 
 
@@ -85,7 +106,7 @@ def write_summary_csv(out_filename, avg_prices):
         None
             Writes a CSV file with header: neighbourhood_group, room_type, average_price
     """
-    pass
+
 
 ###############################################################################
 ##### UNIT TESTS (Do not modify the code below!)
@@ -96,6 +117,7 @@ class TestAirbnbListings(unittest.TestCase):
         full_path = os.path.join(base_path, 'new_york_listings_2024.csv')
         self.listings = load_listings(full_path)
 
+    
     def test_load_listings(self):
         # Test that listings were loaded successfully
         self.assertIsInstance(self.listings, list)
